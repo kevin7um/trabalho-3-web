eb20e65ab3aeea2c7a418a57e6cb00da0240ca87
kevin7um <kevinpestudos@gmail.com>
2024-09-20 15:21:59 -0300

    Criação da api com rotas e funcionalidade para um CRUD de pontos de coletas

--- routes/api.js
+++ routes/api.js
@@ -3,15 +3,17 @@ var router = express.Router();
 const controllerPontos = require('../controller/api/controllerPontos');
 
 //GET
-
+router.get('/consulta/:id', controllerPontos.consulta);
 
 //POST
+router.post('/busca', controllerPontos.busca);
 
+router.post('/cadastro', controllerPontos.cadastro);
 
 //UPDATE
-
+router.put('/atualizacao/:id', controllerPontos.atualiza);
 
 //DELETE
-
+router.delete('/deletar/:id', controllerPontos.deleta);
 
 module.exports = router;
