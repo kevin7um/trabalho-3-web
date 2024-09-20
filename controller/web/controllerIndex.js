766b450982aade0c9ab59771473314decb505cbb
kevin7um <kevinpestudos@gmail.com>
2024-09-20 10:40:56 -0300

    Estruturação e Estilização das páginas Home e Resultado de Busca

--- controller/web/controllerIndex.js
+++ controller/web/controllerIndex.js
@@ -1,8 +1,71 @@
 
 exports.home = function(req, res){
+  const estados = [
+    {uf: "Acre"},
+    {uf: "Alagoas"},
+    {uf: "Amapá"},
+    {uf: "Amazonas"},
+    {uf: "Bahia"},
+    {uf: "Ceará"},
+    {uf: "Distrito Federal"},
+    {uf: "Espirito Santo"},
+    {uf: "Goiás"},
+    {uf: "Maranhão"},
+    {uf: "Mato Grosso do Sul"},
+    {uf: "Mato Grosso"},
+    {uf: "Minas Gerais"},
+    {uf: "Pará"},
+    {uf: "Paraíba"},
+    {uf: "Paraná"},
+    {uf: "Pernambuco"},
+    {uf: "Piauí"},
+    {uf: "Rio de Janeiro"},
+    {uf: "Rio Grande do Norte"},
+    {uf: "Rio Grande do Sul"},
+    {uf: "Rondônia"},
+    {uf: "Roraima"},
+    {uf: "Santa Catarina"},
+    {uf: "São Paulo"},
+    {uf: "Sergipe"},
+    {uf: "Tocantins"},
+  ];
+
   const contexto = {
-    titulo: "Ecoleta - Home"
+    titulo: "Ecoleta - Home",
+    estados: estados,
   }
 
   res.render('index', contexto);
+}
+
+exports.busca = function(req, res){
+
+  const ponto = [
+    {
+      entidade: "Coletoria",
+      endereco: "Avenida JK, Realeza",
+      cidade: "Manhuaçu",
+      estado: "Minas Gerais",
+      numero: 150,
+      itens: ["Resíduos Eletrônicos", "Lâmpadas"]
+    },
+
+    {
+      entidade: "Papersider",
+      endereco: "Rua da Matriz, Realeza",
+      cidade: "Manhuaçu",
+      estado: "Minas Gerais",
+      numero: 100,
+      itens: ["Papel", "Papelão"]
+    },
+
+  ]
+
+  const contexto = {
+    titulo: "Pontos de coletas encontrados",
+    pontos: ponto,
+    quantidade: ponto.length,
+  }
+
+  res.render('busca', contexto);
 }
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,7 @@ const controllerIndex = require('../controller/web/controllerIndex')
 /* GET home page. */
 router.get('/', controllerIndex.home);
 
+/* GET search result */
+router.get('/busca/', controllerIndex.busca)
+
 module.exports = router;
